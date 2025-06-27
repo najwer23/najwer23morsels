28dd7cbed413a98c3dca23dcbb4e811d131f3aa5
Mariusz Najwer <najwer23@gmail.com>
2025-06-27 22:01:24 +0200

    Button sliders

--- src/stories/slider/Slider.stories.tsx
+++ src/stories/slider/Slider.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { action } from 'storybook/actions';
 
 import { Slider } from '.';
 
@@ -18,12 +19,56 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   decorators: [
     () => (
-      <div style={{width: "min(700px,calc(100vw - 50px)", height: "400px"}}>
+      <div style={{ width: 'min(700px,calc(100vw - 50px)', height: '400px' }}>
         <Slider isCircular>
-          <div style={{background: "red", display: "flex", justifyContent: "center", alignItems: "center", color: "white", fontSize: "40px"}}>1</div>
-          <div style={{background: "blue", display: "flex", justifyContent: "center", alignItems: "center", color: "white", fontSize: "40px"}}>2</div>
-          <div style={{background: "orange", display: "flex", justifyContent: "center", alignItems: "center" , color: "white", fontSize: "40px"}}>3</div>
-          <div style={{background: "green", display: "flex", justifyContent: "center", alignItems: "center", color: "white", fontSize: "40px"}}>4</div>
+          <div
+            style={{
+              background: 'red',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: 'white',
+              fontSize: '40px',
+            }}
+            onClick={action('on-click-1')}>
+            1
+          </div>
+          <div
+          onClick={action('on-click-2')}
+            style={{
+              background: 'blue',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: 'white',
+              fontSize: '40px',
+            }}>
+            2
+          </div>
+          <div
+          onClick={action('on-click-3')}
+            style={{
+              background: 'orange',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: 'white',
+              fontSize: '40px',
+            }}>
+            3
+          </div>
+          <div
+          onClick={action('on-click-4')}
+            style={{
+              background: 'green',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: 'white',
+              fontSize: '40px',
+            }}>
+            4
+          </div>
         </Slider>
       </div>
     ),
