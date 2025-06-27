86c8ed1afac707091020f7e82ca0b39281a9ca82
Mariusz Najwer <najwer23@gmail.com>
2025-06-27 22:38:27 +0200

    Arrows svg

--- src/stories/slider/Slider.tsx
+++ src/stories/slider/Slider.tsx
@@ -15,7 +15,7 @@ const cloneSlides = (slides: SlideElement[], count: number, fromStart = false): 
       className: el.props.className,
     }),
   );
-}
+};
 
 export const Slider: React.FC<SliderProps> = ({ isCircular = false, children }) => {
   const childSlides = React.Children.toArray(children).filter(React.isValidElement) as SlideElement[];
@@ -145,10 +145,12 @@ export const Slider: React.FC<SliderProps> = ({ isCircular = false, children }) 
     <div className={styles.sliderContainer}>
       <div className={styles.sliderBtnControl}>
         <button className={styles.btn} onClick={prevSlide} disabled={isAnimating}>
-          Prev
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="24" height="24" fill="currentColor"><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
         </button>
         <button className={styles.btn} onClick={nextSlide} disabled={isAnimating}>
-          Next
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="24" height="24" fill="currentColor">
+            <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+          </svg>
         </button>
       </div>
       <div className={styles.slideWrapper} ref={slideWrapperRef}>
