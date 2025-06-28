eb1cd50e74b0d83adfdf8178ba6e57d4c47676d7
Mariusz Najwer <najwer23@gmail.com>
2025-06-28 08:45:09 +0200

    Updates

---
+++ assets/TextBox.stories-6OmscAOk.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";const w="_morselsTextBox_1f7yj_2",v={morselsTextBox:w},e=({tag:u="div",children:a,className:c,target:l,rel:p,mobileSize:g,desktopSize:x,lineHeight:m,color:f,href:n,colorHover:h,margin:i,fontWeight:k,fontWeightHover:y,...b})=>{const j=u;return o.jsxs(j,{style:{"--fw":k,"--fwh":y,"--ch":h,"--c":f,"--ms":g,"--ds":x,"--lh":m,...i!=null?{"--m":i}:{}},className:[v.morselsTextBox,"MorselsTextBox",c].filter(Boolean).join(" "),...b,children:[n&&o.jsx("a",{className:"MorselsTextBoxLink",href:n,target:l,rel:p,children:a}),!n&&a]})};e.displayName="TextBox";e.__docgenInfo={description:"",methods:[],displayName:"TextBox",props:{tag:{defaultValue:{value:"'div'",computed:!1},required:!1}}};const S={title:"TextBox/TextBox",component:e,parameters:{layout:"centered"},tags:["autodocs"]},r={decorators:[()=>o.jsx("div",{children:o.jsx(e,{color:"grey",mobileSize:20,desktopSize:20,children:"It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to."})})]},s={decorators:[()=>o.jsx("div",{children:o.jsx(e,{color:"green",colorHover:"orange",mobileSize:20,desktopSize:20,href:"/ksdjdkdj",children:"It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to."})})]},t={decorators:[()=>o.jsx("div",{children:o.jsx(e,{color:"red",colorHover:"orange",mobileSize:20,desktopSize:40,href:"/ksdjdkdj",children:"It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to."})})]},d={decorators:[()=>o.jsx("div",{children:o.jsx(e,{color:"red",colorHover:"orange",mobileSize:40,desktopSize:20,href:"/ksdjdkdj",children:"It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to."})})]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  decorators: [() => <div>
+        <TextBox color="grey" mobileSize={20} desktopSize={20}>
+          It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your
+          feet, there's no knowing where you might be swept off to.
+        </TextBox>
+      </div>]
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  decorators: [() => <div>
+        <TextBox color="green" colorHover="orange" mobileSize={20} desktopSize={20} href="/ksdjdkdj">
+          It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your
+          feet, there's no knowing where you might be swept off to.
+        </TextBox>
+      </div>]
+}`,...s.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  decorators: [() => <div>
+        <TextBox color="red" colorHover="orange" mobileSize={20} desktopSize={40} href="/ksdjdkdj">
+          It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your
+          feet, there's no knowing where you might be swept off to.
+        </TextBox>
+      </div>]
+}`,...t.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  decorators: [() => <div>
+        <TextBox color={'red'} colorHover={'orange'} mobileSize={40} desktopSize={20} href="/ksdjdkdj">
+          It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your
+          feet, there's no knowing where you might be swept off to.
+        </TextBox>
+      </div>]
+}`,...d.parameters?.docs?.source}}};const T=["Default","Link","FluidMobile20Desktop40","FluidMobile40Desktop20"];export{r as Default,t as FluidMobile20Desktop40,d as FluidMobile40Desktop20,s as Link,T as __namedExportsOrder,S as default};
