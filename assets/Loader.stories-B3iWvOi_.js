efc8cbd78081d0588f56a5975cd51a9290730b12
Mariusz Najwer <najwer23@gmail.com>
2025-06-28 23:38:25 +0200

    Updates

---
+++ assets/Loader.stories-B3iWvOi_.js
@@ -0,0 +1,8 @@
+import{j as r}from"./TextBox-CzriwBW6.js";import{L as t}from"./Loader-NOt-5lqn.js";const s={title:"Loader/Loader",component:t,parameters:{layout:"centered"},tags:["autodocs"]},e={decorators:[()=>r.jsx("div",{style:{width:"min(700px,calc(100vw - 50px)",height:"400px"},children:r.jsx(t,{})})]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  decorators: [() => <div style={{
+    width: 'min(700px,calc(100vw - 50px)',
+    height: '400px'
+  }}>
+        <Loader />
+      </div>]
+}`,...e.parameters?.docs?.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,s as default};
---
+++ assets/TextBox.stories-qabioKTJ.js
@@ -0,0 +1,47 @@
+import{T as o,j as e}from"./TextBox-CzriwBW6.js";const c={title:"TextBox/TextBox",component:o,parameters:{layout:"centered"},tags:["autodocs"]},t="It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",s={args:{color:"grey",mobileSize:20,desktopSize:20,children:t},render:r=>e.jsx("div",{children:e.jsx(o,{...r})})},n={args:{color:"green",colorHover:"orange",mobileSize:20,desktopSize:20,href:"/ksdjdkdj",children:t},render:r=>e.jsx("div",{children:e.jsx(o,{...r})})},d={args:{color:"red",colorHover:"orange",mobileSize:20,desktopSize:40,href:"/ksdjdkdj",children:t},render:r=>e.jsx("div",{children:e.jsx(o,{...r})})},a={args:{color:"red",colorHover:"orange",mobileSize:40,desktopSize:20,href:"/ksdjdkdj",children:t},render:r=>e.jsx("div",{children:e.jsx(o,{...r})})};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'grey',
+    mobileSize: 20,
+    desktopSize: 20,
+    children: defaultText
+  },
+  render: args => <div>
+      <TextBox {...args} />
+    </div>
+}`,...s.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'green',
+    colorHover: 'orange',
+    mobileSize: 20,
+    desktopSize: 20,
+    href: '/ksdjdkdj',
+    children: defaultText
+  },
+  render: args => <div>
+      <TextBox {...args} />
+    </div>
+}`,...n.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'red',
+    colorHover: 'orange',
+    mobileSize: 20,
+    desktopSize: 40,
+    href: '/ksdjdkdj',
+    children: defaultText
+  },
+  render: args => <div>
+      <TextBox {...args} />
+    </div>
+}`,...d.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'red',
+    colorHover: 'orange',
+    mobileSize: 40,
+    desktopSize: 20,
+    href: '/ksdjdkdj',
+    children: defaultText
+  },
+  render: args => <div>
+      <TextBox {...args} />
+    </div>
+}`,...a.parameters?.docs?.source}}};const l=["Default","Link","FluidMobile20Desktop40","FluidMobile40Desktop20"];export{s as Default,d as FluidMobile20Desktop40,a as FluidMobile40Desktop20,n as Link,l as __namedExportsOrder,c as default};
