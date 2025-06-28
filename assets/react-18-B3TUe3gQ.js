2ecfa9026ed8a25e9f840c7a7d1324cfd30424ed
Mariusz Najwer <najwer23@gmail.com>
2025-06-28 23:39:12 +0200

    Updates

--- assets/react-18-B3TUe3gQ.js
+++ assets/react-18-B3TUe3gQ.js
@@ -1,4 +1,4 @@
-import{a as k1,r as Fc}from"./iframe-B_v79YXm.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as k1,r as Fc}from"./iframe-DjiA_r8r.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
