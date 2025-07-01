0599ea46c7fa181ca9f4d3f03d42bacc949a9ae0
Mariusz Najwer <najwer23@gmail.com>
2025-07-01 22:38:36 +0200

    Add accessibility support with @storybook/addon-a11y and update configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,9 +3,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   staticDirs: ['../public'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
+  addons: ['@storybook/addon-a11y']
 };
 export default config;
