4e2984cd4f1d57c6b75f2fed508fa6af9e54df0c
Mariusz Najwer <najwer23@gmail.com>
2025-07-01 23:32:36 +0200

    Updates

---
+++ assets/Loader.stories-D6Z1FVhY.js
@@ -0,0 +1,8 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{L as t}from"./Loader-DGIwYgTZ.js";import"./TextBox-DUyBA2BJ.js";const d={title:"Loader/Loader",component:t,parameters:{layout:"centered"},tags:["autodocs"]},e={decorators:[()=>r.jsx("div",{style:{width:"min(700px,calc(100vw - 50px)",height:"400px"},children:r.jsx(t,{})})]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  decorators: [() => <div style={{
+    width: 'min(700px,calc(100vw - 50px)',
+    height: '400px'
+  }}>
+        <Loader />
+      </div>]
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,d as default};
--- assets/Loader.stories-D_YX6Zf6.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{L as t}from"./Loader-C4fRsJDM.js";import"./TextBox-C5QJzpaH.js";const d={title:"Loader/Loader",component:t,parameters:{layout:"centered"},tags:["autodocs"]},e={decorators:[()=>r.jsx("div",{style:{width:"min(700px,calc(100vw - 50px)",height:"400px"},children:r.jsx(t,{})})]};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  decorators: [() => <div style={{
-    width: 'min(700px,calc(100vw - 50px)',
-    height: '400px'
-  }}>
-        <Loader />
-      </div>]
-}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,d as default};
