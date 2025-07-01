6d1b06bdabe277822077b9f6c75e794efa834d5d
Mariusz Najwer <najwer23@gmail.com>
2025-07-01 07:48:12 +0200

    Updates

---
+++ assets/CodeParser.stories-D6jNq2Zd.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";const t="_najwer23morselsCodeParserContainer_1msen_40",i={najwer23morselsCodeParserContainer:t},a=({children:n,className:o,...s})=>e.jsx("div",{className:[i.najwer23morselsCodeParserContainer,"MorselsCodeParser",o].filter(Boolean).join(" "),...s,children:e.jsx("pre",{children:e.jsx("code",{children:n})})});a.displayName="CodeParser";a.__docgenInfo={description:"",methods:[],displayName:"CodeParser",props:{children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const c={title:"CodeParser/CodeParser",component:a,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{},render:n=>e.jsx("div",{children:e.jsx(a,{...n,children:`@keyframes rounded-mask-reveal {
+  from {
+    clip-path: inset(10% 10% 10% 10% round 30px);
+  }
+  to {
+    clip-path: inset(0% 0% 0% 0% round 30px);
+  }
+}
+
+video {
+  animation: rounded-mask-reveal linear both;
+  animation-timeline: view();
+  animation-range: cover 0% cover 50%;
+}`})})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div>
+      <CodeParser {...args}>{\`@keyframes rounded-mask-reveal {
+  from {
+    clip-path: inset(10% 10% 10% 10% round 30px);
+  }
+  to {
+    clip-path: inset(0% 0% 0% 0% round 30px);
+  }
+}
+
+video {
+  animation: rounded-mask-reveal linear both;
+  animation-timeline: view();
+  animation-range: cover 0% cover 50%;
+}\`}</CodeParser>
+    </div>
+}`,...r.parameters?.docs?.source}}};const m=["Default"];export{r as Default,m as __namedExportsOrder,c as default};
