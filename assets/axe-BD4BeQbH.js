992ed8bde2ed5fe86c059d92aaecd8231137ec3b
Mariusz Najwer <najwer23@gmail.com>
2025-07-02 17:48:39 +0200

    Updates

--- assets/axe-BD4BeQbH.js
+++ assets/axe-BD4BeQbH.js
@@ -1,4 +1,4 @@
-import{g as MT}from"./iframe-r4hLeYsE.js";var mi={exports:{}};/*! axe v4.10.3
+import{g as MT}from"./iframe-71e_Leyc.js";var mi={exports:{}};/*! axe v4.10.3
  * Copyright (c) 2015 - 2025 Deque Systems, Inc.
  *
  * Your use of this Source Code Form is subject to the terms of the Mozilla Public
