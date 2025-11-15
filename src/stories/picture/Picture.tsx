2cb4773d1375a2caddb7fedafece07cdb24e25b3
Mariusz Najwer <najwer23@gmail.com>
2025-11-15 21:19:49 +0100

    Border picture

--- src/stories/picture/Picture.tsx
+++ src/stories/picture/Picture.tsx
@@ -9,19 +9,34 @@ interface PictureProps extends React.HTMLAttributes<HTMLPictureElement> {
   alt: string;
   ar?: number;
   draggable?: boolean;
+  border?: boolean;
+  borderColor?: string;
 }
 
-export const Picture: React.FC<PictureProps> = ({ children, className, style, draggable, alt, src, ar, ...props }) => {
+export const Picture: React.FC<PictureProps> = ({
+  children,
+  className,
+  style,
+  draggable = false,
+  alt,
+  src,
+  ar,
+  border = false,
+  borderColor = 'black',
+  ...props
+}) => {
   const [loaded, setLoaded] = useState(false);
 
   return (
     <picture
-      className={[styles.n23mPicture, 'n23mPicture', loaded && styles.loaded, className].filter(Boolean).join(' ')}
+      className={[styles.n23mPicture, 'n23mPicture', border && styles.border, loaded && styles.loaded, className]
+        .filter(Boolean)
+        .join(' ')}
       {...props}
       style={
         {
           ...getCssVariableStyle({
-            '--picture': 'test',
+            '--picture-bc': borderColor,
           }),
           ...style,
         } as React.CSSProperties
