319b75ea88c6d0d0b33a93045fb71b4f84ebd7c8
Mariusz Najwer <najwer23@gmail.com>
2026-04-02 12:04:03 +0200

    Updates

---
+++ assets/CodeParser.stories-CY11RB6M.js
@@ -0,0 +1,39 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{S as t}from"./iframe-BvAeNPTh.js";import{t as n}from"./jsx-runtime-Bn1Ys6_W.js";var r,i,a=e((()=>{r=`_n23mCodeParser_t5r6o_36`,i={"--n23mCodeParserPalette":` `,n23mCodeParser:r}})),o,s,c=e((()=>{t(),a(),o=n(),s=({children:e,className:t,...n})=>(0,o.jsx)(`div`,{className:[i.n23mCodeParser,`n23mCodeParser`,t].filter(Boolean).join(` `),...n,children:(0,o.jsx)(`pre`,{children:(0,o.jsx)(`code`,{children:e})})}),s.displayName=`CodeParser`,s.__docgenInfo={description:``,methods:[],displayName:`CodeParser`,props:{children:{required:!1,tsType:{name:`ReactReactNode`,raw:`React.ReactNode`},description:``}}}})),l=e((()=>{c()})),u,d,f,p;e((()=>{l(),u=n(),d={title:`CodeParser/CodeParser`,component:s,parameters:{layout:`centered`,docs:{description:{component:`
+This is a CodeParser component that can be used to display code snippets with syntax highlighting. It supports various properties such as language, theme, and more.
+
+You can import the CodeParser component from the library and use it in your application as follows:
+\`\`\`tsx
+import { CodeParser } from 'najwer23morsels/lib/codeparser';
+\`\`\`
+      `}}},tags:[`autodocs`]},f={args:{},render:e=>(0,u.jsx)(`div`,{children:(0,u.jsx)(s,{...e,children:`@keyframes rounded-mask-reveal {
+  from {
+    clip-path: inset(10% 10% 10% 10% round 30px);
+  }
+  to {
+    clip-path: inset(0% 0% 0% 0% round 30px);
+  }
+}
+
+video {
+  animation: rounded-mask-reveal linear both;
+  animation-timeline: view();
+  animation-range: cover 0% cover 50%;
+}`})})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div>
+      <CodeParser {...args}>{\`@keyframes rounded-mask-reveal {
+  from {
+    clip-path: inset(10% 10% 10% 10% round 30px);
+  }
+  to {
+    clip-path: inset(0% 0% 0% 0% round 30px);
+  }
+}
+
+video {
+  animation: rounded-mask-reveal linear both;
+  animation-timeline: view();
+  animation-range: cover 0% cover 50%;
+}\`}</CodeParser>
+    </div>
+}`,...f.parameters?.docs?.source}}},p=[`Default`]}))();export{f as Default,p as __namedExportsOrder,d as default};
---
+++ assets/Form.stories-BDzliT7f.js
@@ -0,0 +1,58 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{S as n}from"./iframe-BvAeNPTh.js";import{t as r}from"./jsx-runtime-Bn1Ys6_W.js";import{n as i,t as a}from"./textbox-1qXqhfCz.js";import{n as o,t as s}from"./button-BrP5yn9o.js";import{n as c,t as l}from"./input-BGrlgoeb.js";var u,d,f=t((()=>{a(),u=r(),d=({children:e,onSubmit:t,className:n,isError:r,isPending:a,isSuccess:o,errorMsg:s,successMsg:c,style:l,...d})=>{let f=e=>{e.preventDefault();let n=e.currentTarget;n&&Array.from(n.elements).forEach(e=>{(e instanceof HTMLInputElement||e instanceof HTMLTextAreaElement||e instanceof HTMLSelectElement)&&e.dispatchEvent(new FocusEvent(`focusout`,{bubbles:!0}))}),t(Object.fromEntries(Array.from(n.elements).filter(e=>e instanceof HTMLInputElement||e instanceof HTMLTextAreaElement||e instanceof HTMLSelectElement&&e.name!==``).map(e=>[e.name,{error:e.className.includes(`error`),value:e.value}])))};return(0,u.jsxs)(`form`,{className:[`n23mForm`,n].filter(Boolean).join(` `),...d,style:{...l},onSubmit:f,children:[e,r&&(0,u.jsx)(i,{tag:`p`,color:`#ff3333`,mobileSize:12,desktopSize:12,margin:`15px 0 10px 0`,fontWeight:400,children:s}),a&&(0,u.jsx)(i,{tag:`p`,color:`grey`,mobileSize:12,desktopSize:12,margin:`15px 0 10px 0`,fontWeight:400,children:`Loading..`}),o&&(0,u.jsx)(i,{tag:`p`,color:`#4BB543`,mobileSize:12,desktopSize:12,margin:`15px 0 10px 0`,fontWeight:400,children:c})]})},d.displayName=`Form`,d.__docgenInfo={description:``,methods:[],displayName:`Form`}})),p=t((()=>{f()})),m,h,g,_,v,y;t((()=>{m=e(n(),1),s(),l(),a(),p(),h=r(),{action:g}=__STORYBOOK_MODULE_ACTIONS__,_={title:`Form/Form`,component:d,parameters:{layout:`centered`,docs:{description:{component:`
+The Form component provides a structured way to handle form submission with validation support. It utilizes controlled inputs and custom validation rules to manage user input efficiently.
+
+\`\`\`tsx
+import { Form } from 'najwer23morsels/lib/form';
+\`\`\`
+      `}}},tags:[`autodocs`]},v={args:{onSubmit:()=>{},children:(0,h.jsx)(h.Fragment,{})},render:e=>{let[t,n]=(0,m.useState)(!1),[r,a]=(0,m.useState)(!1),[s,l]=(0,m.useState)(!1);return(0,h.jsx)(h.Fragment,{children:(0,h.jsxs)(d,{onSubmit:e=>{n(!1),a(!1),l(!0),setTimeout(()=>{if(l(!1),g(`Form:`,e)(e),Object.values(e).some(({error:e})=>e)){g(`Form has errors`)(),a(!0);return}g(`Form submitted`)(),n(!0)},500)},isError:r,isPending:s,isSuccess:t,errorMsg:`Form has errors`,successMsg:`Form submitted`,children:[(0,h.jsx)(c,{label:`Email`,type:`text`,name:`name`,validatorOptions:[{type:`empty`},{type:`email`}]}),(0,h.jsx)(c,{label:`Password`,type:`password`,name:`pass`,validatorOptions:[{type:`empty`}]}),(0,h.jsx)(c,{kind:`textarea`,label:`Msg`,type:`text`,name:`msg`,validatorOptions:[{type:`empty`}]}),(0,h.jsx)(o,{type:`submit`,backgroundColor:`yellow`,borderColor:`black`,height:`40px`,padding:0,width:`300px`,children:(0,h.jsx)(i,{mobileSize:18,desktopSize:18,children:`Submit`})})]})})}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    onSubmit: () => {},
+    children: <></>
+  },
+  render: args => {
+    const [isSuccess, setIsSucess] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [isPending, setIsPending] = useState(false);
+    const handleOnSubmit = (form: FormType) => {
+      setIsSucess(false);
+      setIsError(false);
+      setIsPending(true);
+      setTimeout(() => {
+        setIsPending(false);
+        action('Form:', form)(form);
+        if (Object.values(form).some(({
+          error
+        }) => error)) {
+          action('Form has errors')();
+          setIsError(true);
+          return;
+        }
+        action('Form submitted')();
+        setIsSucess(true);
+      }, 500);
+    };
+    return <>
+        <Form onSubmit={handleOnSubmit} isError={isError} isPending={isPending} isSuccess={isSuccess} errorMsg={'Form has errors'} successMsg={'Form submitted'}>
+          <Input label="Email" type="text" name="name" validatorOptions={[{
+          type: 'empty'
+        }, {
+          type: 'email'
+        }]} />
+
+          <Input label="Password" type="password" name="pass" validatorOptions={[{
+          type: 'empty'
+        }]} />
+
+          <Input kind="textarea" label="Msg" type="text" name="msg" validatorOptions={[{
+          type: 'empty'
+        }]} />
+
+          <Button type="submit" backgroundColor="yellow" borderColor="black" height="40px" padding={0} width="300px">
+            <TextBox mobileSize={18} desktopSize={18}>
+              Submit
+            </TextBox>
+          </Button>
+        </Form>
+      </>;
+  }
+}`,...v.parameters?.docs?.source}}},y=[`Default`]}))();export{v as Default,y as __namedExportsOrder,_ as default};
