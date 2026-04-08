18f6174cc2854ec5806509a5a3c596cd26af9996
Mariusz Najwer <najwer23@gmail.com>
2026-04-08 19:56:57 +0200

    Updates

---
+++ assets/Grid.stories-qbecOUsv.js
@@ -0,0 +1,124 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{S as n}from"./iframe-DzavuLSr.js";import{t as r}from"./jsx-runtime-D16BNjX-.js";import{n as i,t as a}from"./textbox-D6U7cgeO.js";import{n as o,t as s}from"./Grid-BFflqkc_.js";var c,l,u,d,f,p,m,h,g;t((()=>{c=e(n(),1),a(),o(),l=r(),u={title:`Grid/Grid`,component:s,parameters:{layout:`centered`,docs:{description:{component:`
+This is a grid component that can be used to create responsive layouts. It supports various properties such as layout type, column configuration, gap, padding, and more. You can use it to create both container-based and column-based layouts.
+
+You can import the Grid component from the library and use it in your application as follows:
+\`\`\`tsx
+import { Grid } from 'najwer23morsels/lib/grid';
+\`\`\`
+      `}}},tags:[`autodocs`]},d=`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,f={args:{layout:`container`,widthMax:`600px`,gap:{col:`20px`,row:`20px`},padding:`10px`},render:e=>(0,l.jsx)(`div`,{style:{width:`100vw`,backgroundColor:`#f0f0f0`},children:(0,l.jsx)(s,{...e,children:(0,l.jsx)(`div`,{style:{border:`1px solid black`,backgroundColor:`#f0f0f0`},children:(0,l.jsx)(i,{mobileSize:12,desktopSize:12,children:`Padding: 10px; max-width: 600px`})})})})},p={args:{layout:`container`,widthMax:`600px`,padding:`10px`,minHeight:`402px`},render:e=>{let[t,n]=(0,c.useState)(!0);return(0,c.useEffect)(()=>{let e=setInterval(()=>{n(e=>!e)},3e3);return()=>clearInterval(e)},[]),(0,l.jsx)(`div`,{style:{width:`100vw`,backgroundColor:`#f0f0f0`},children:(0,l.jsx)(s,{...e,loading:t,children:(0,l.jsxs)(`div`,{style:{minHeight:`400px`,backgroundColor:`#f0f0f0`,border:`1px solid black`},children:[(0,l.jsx)(i,{mobileSize:12,desktopSize:12,children:`max-width: 600px`}),(0,l.jsx)(i,{mobileSize:12,desktopSize:12,children:`Padding: 10px; minHeight: 400px`})]})})})}},m={args:{layout:`columns`,gap:{col:`20px`,row:`20px`},col:{smallDesktop:3,desktop:3,mobile:1,tablet:2}},render:e=>(0,l.jsx)(`div`,{style:{width:`600px`},children:(0,l.jsx)(s,{...e,children:Array.from({length:9},(e,t)=>(0,l.jsx)(`div`,{children:(0,l.jsx)(i,{mobileSize:16,desktopSize:16,children:d})},t))})})},h={args:{layout:`columns`,gap:{col:`20px`,row:`20px`},col:{smallDesktop:2,desktop:2,mobile:1,tablet:2}},render:e=>(0,l.jsx)(`div`,{style:{width:`100vw`},children:(0,l.jsx)(s,{layout:`container`,widthMax:`400px`,padding:`0 10px 0 10px`,children:(0,l.jsx)(s,{...e,children:Array.from({length:9},(e,t)=>(0,l.jsx)(`div`,{children:(0,l.jsx)(i,{mobileSize:16,desktopSize:16,children:d})},t))})})})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: 'container',
+    widthMax: '600px',
+    gap: {
+      col: '20px',
+      row: '20px'
+    },
+    padding: '10px'
+  },
+  render: args => <div style={{
+    width: '100vw',
+    backgroundColor: '#f0f0f0'
+  }}>
+      <Grid {...args}>
+        <div style={{
+        border: '1px solid black',
+        backgroundColor: '#f0f0f0'
+      }}>
+          <TextBox mobileSize={12} desktopSize={12}>
+            Padding: 10px; max-width: 600px
+          </TextBox>
+        </div>
+      </Grid>
+    </div>
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: 'container',
+    widthMax: '600px',
+    padding: '10px',
+    minHeight: '402px'
+  },
+  render: args => {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setLoading(prev => !prev);
+      }, 3000);
+      return () => clearInterval(interval);
+    }, []);
+    return <div style={{
+      width: '100vw',
+      backgroundColor: '#f0f0f0'
+    }}>
+        <Grid {...args} loading={loading}>
+          <div style={{
+          minHeight: '400px',
+          backgroundColor: '#f0f0f0',
+          border: '1px solid black'
+        }}>
+            <TextBox mobileSize={12} desktopSize={12}>
+              max-width: 600px
+            </TextBox>
+            <TextBox mobileSize={12} desktopSize={12}>
+              Padding: 10px; minHeight: 400px
+            </TextBox>
+          </div>
+        </Grid>
+      </div>;
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: 'columns',
+    gap: {
+      col: '20px',
+      row: '20px'
+    },
+    col: {
+      smallDesktop: 3,
+      desktop: 3,
+      mobile: 1,
+      tablet: 2
+    }
+  },
+  render: args => <div style={{
+    width: '600px'
+  }}>
+      <Grid {...args}>
+        {Array.from({
+        length: 9
+      }, (_, i) => <div key={i}>
+            <TextBox mobileSize={16} desktopSize={16}>
+              {testText}
+            </TextBox>
+          </div>)}
+      </Grid>
+    </div>
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: 'columns',
+    gap: {
+      col: '20px',
+      row: '20px'
+    },
+    col: {
+      smallDesktop: 2,
+      desktop: 2,
+      mobile: 1,
+      tablet: 2
+    }
+  },
+  render: args => <div style={{
+    width: '100vw'
+  }}>
+      <Grid layout="container" widthMax={'400px'} padding="0 10px 0 10px">
+        <Grid {...args}>
+          {Array.from({
+          length: 9
+        }, (_, i) => <div key={i}>
+              <TextBox mobileSize={16} desktopSize={16}>
+                {testText}
+              </TextBox>
+            </div>)}
+        </Grid>
+      </Grid>
+    </div>
+}`,...h.parameters?.docs?.source}}},g=[`GridContainerWidthMax600`,`GridContainerLoading`,`GridColumns`,`GridContainerWithColumns`]}))();export{m as GridColumns,p as GridContainerLoading,f as GridContainerWidthMax600,h as GridContainerWithColumns,g as __namedExportsOrder,u as default};
---
+++ assets/select-HJucJAy6.js
@@ -0,0 +1,44 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{S as n}from"./iframe-DzavuLSr.js";import{t as r}from"./jsx-runtime-D16BNjX-.js";import{n as i,t as a}from"./input-DUl-8jDF.js";var o,s,c,l,u,d,f,p,m=t((()=>{o=`_n23mSelect_1e1z3_2`,s=`_icon_1e1z3_14`,c=`_iconRotate_1e1z3_38`,l=`_dropdownWrapper_1e1z3_44`,u=`_open_1e1z3_54`,d=`_dropdown_1e1z3_44`,f=`_dropdownItem_1e1z3_77`,p={n23mSelect:o,icon:s,iconRotate:c,dropdownWrapper:l,open:u,dropdown:d,dropdownItem:f}})),h,g,_,v=t((()=>{h=e(n(),1),a(),m(),g=r(),_=({className:e,name:t,disabled:n,style:r,options:a,initialValue:o,validatorOptions:s,placeholder:c,onBlur:l,label:u=`Select`,...d})=>{let[f,m]=(0,h.useState)(o?.label??``),[_,v]=(0,h.useState)(o?.value??``),[y,b]=(0,h.useState)(!1),x=(0,h.useRef)(null),S=(0,h.useId)(),C=(0,h.useMemo)(()=>a.filter(e=>e.label.toLowerCase().includes(f.toLowerCase())),[a,f]),w=e=>{let t=e.currentTarget.value;m(t),v(``),b(!0)},T=()=>{b(e=>!e),m(``),v(``)},E=e=>{b(!1),l?.(e,{value:_,label:f})},D=e=>{v(e.value),m(e.label),b(!1)};return(0,g.jsxs)(`div`,{className:[p.n23mSelect,`n23mSelect`,e].filter(Boolean).join(` `),...d,style:{...r},children:[(0,g.jsx)(`div`,{className:[p.icon,y?p.iconRotate:``].join(` `),onMouseDown:e=>{e.preventDefault(),x.current?.focus(),b(e=>!e)}}),(0,g.jsx)(i,{name:t,type:`hidden`,kind:`input`,validatorOptions:[...s??[],{type:`existsInOptions`,options:{options:a,matchBy:`value`}}],value:_}),(0,g.jsx)(i,{id:S,inputRef:x,label:u,type:`text`,kind:`select`,disabled:n,placeholder:c,value:f,onChange:w,onClick:T,onBlur:E,validatorOptions:[...s??[],{type:`existsInOptions`,options:{options:a,matchBy:`label`}}],"aria-autocomplete":`list`,"aria-expanded":y,"aria-controls":`${t}-listbox`,role:`combobox`}),(0,g.jsx)(`div`,{className:[p.dropdownWrapper,y?p.open:``].join(` `),children:(0,g.jsx)(`div`,{className:[p.dropdown].join(` `),children:C.map(e=>(0,g.jsx)(`div`,{className:p.dropdownItem,onMouseDown:t=>{D(e)},children:e.label},e.value))})})]})},_.__docgenInfo={description:``,methods:[],displayName:`Select`,props:{style:{required:!1,tsType:{name:`ReactCSSProperties`,raw:`React.CSSProperties`},description:``},name:{required:!0,tsType:{name:`string`},description:``},options:{required:!0,tsType:{name:`Array`,elements:[{name:`signature`,type:`object`,raw:`{
+  value: string;
+  label: string;
+}`,signature:{properties:[{key:`value`,value:{name:`string`,required:!0}},{key:`label`,value:{name:`string`,required:!0}}]}}],raw:`Option[]`},description:``},inputRef:{required:!1,tsType:{name:`ReactRefObject`,raw:`React.RefObject<HTMLInputElement | null>`,elements:[{name:`union`,raw:`HTMLInputElement | null`,elements:[{name:`HTMLInputElement`},{name:`null`}]}]},description:``},initialValue:{required:!1,tsType:{name:`signature`,type:`object`,raw:`{
+  value: string;
+  label: string;
+}`,signature:{properties:[{key:`value`,value:{name:`string`,required:!0}},{key:`label`,value:{name:`string`,required:!0}}]}},description:``},validatorOptions:{required:!1,tsType:{name:`Array`,elements:[{name:`union`,raw:`| { type: 'empty' }
+| { type: 'email' }
+| { type: 'numberInRange'; options?: { min?: number; max?: number } }
+| { type: 'date' }
+| {
+    type: 'existsInOptions';
+    options: {
+      options: {
+        value: string;
+        label: string;
+      }[];
+      matchBy?: 'value' | 'label';
+    };
+  }`,elements:[{name:`signature`,type:`object`,raw:`{ type: 'empty' }`,signature:{properties:[{key:`type`,value:{name:`literal`,value:`'empty'`,required:!0}}]}},{name:`signature`,type:`object`,raw:`{ type: 'email' }`,signature:{properties:[{key:`type`,value:{name:`literal`,value:`'email'`,required:!0}}]}},{name:`signature`,type:`object`,raw:`{ type: 'numberInRange'; options?: { min?: number; max?: number } }`,signature:{properties:[{key:`type`,value:{name:`literal`,value:`'numberInRange'`,required:!0}},{key:`options`,value:{name:`signature`,type:`object`,raw:`{ min?: number; max?: number }`,signature:{properties:[{key:`min`,value:{name:`number`,required:!1}},{key:`max`,value:{name:`number`,required:!1}}]},required:!1}}]}},{name:`signature`,type:`object`,raw:`{ type: 'date' }`,signature:{properties:[{key:`type`,value:{name:`literal`,value:`'date'`,required:!0}}]}},{name:`signature`,type:`object`,raw:`{
+  type: 'existsInOptions';
+  options: {
+    options: {
+      value: string;
+      label: string;
+    }[];
+    matchBy?: 'value' | 'label';
+  };
+}`,signature:{properties:[{key:`type`,value:{name:`literal`,value:`'existsInOptions'`,required:!0}},{key:`options`,value:{name:`signature`,type:`object`,raw:`{
+  options: {
+    value: string;
+    label: string;
+  }[];
+  matchBy?: 'value' | 'label';
+}`,signature:{properties:[{key:`options`,value:{name:`Array`,elements:[{name:`signature`,type:`object`,raw:`{
+  value: string;
+  label: string;
+}`,signature:{properties:[{key:`value`,value:{name:`string`,required:!0}},{key:`label`,value:{name:`string`,required:!0}}]}}],raw:`{
+  value: string;
+  label: string;
+}[]`,required:!0}},{key:`matchBy`,value:{name:`union`,raw:`'value' | 'label'`,elements:[{name:`literal`,value:`'value'`},{name:`literal`,value:`'label'`}],required:!1}}]},required:!0}}]}}]}],raw:`ValidatorOption[]`},description:``},label:{required:!1,tsType:{name:`string`},description:``,defaultValue:{value:`'Select'`,computed:!1}},placeholder:{required:!1,tsType:{name:`string`},description:``},disabled:{required:!1,tsType:{name:`boolean`},description:``},onBlur:{required:!1,tsType:{name:`signature`,type:`function`,raw:`(
+  e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
+  data: { value: string; label: string },
+) => void`,signature:{arguments:[{type:{name:`ReactFocusEvent`,raw:`React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>`,elements:[{name:`union`,raw:`HTMLInputElement | HTMLTextAreaElement`,elements:[{name:`HTMLInputElement`},{name:`HTMLTextAreaElement`}]}]},name:`e`},{type:{name:`signature`,type:`object`,raw:`{ value: string; label: string }`,signature:{properties:[{key:`value`,value:{name:`string`,required:!0}},{key:`label`,value:{name:`string`,required:!0}}]}},name:`data`}],return:{name:`void`}}},description:``}},composes:[`Omit`]}})),y=t((()=>{v()}));export{_ as n,y as t};
