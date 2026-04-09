e4f34a09a31f42fcd60a4e2da519a86d6b8fc339
Mariusz Najwer <najwer23@gmail.com>
2026-04-09 13:54:09 +0200

    Footer params

--- src/stories/footer/Footer.tsx
+++ src/stories/footer/Footer.tsx
@@ -6,16 +6,29 @@ import styles from './Footer.module.css';
 interface FooterProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
   style?: React.CSSProperties;
+  author?: string;
+  initialYear?: number;
+  color: string;
+  backgroundColor?: string;
 }
 
-export const Footer: React.FC<FooterProps> = ({ children, className, style, ...props }) => {
+export const Footer: React.FC<FooterProps> = ({
+  children,
+  author = 'Mariusz Najwer',
+  className,
+  initialYear = '2019',
+  backgroundColor = '#333333',
+  color = 'white',
+  style,
+  ...props
+}) => {
   return (
     <footer
       className={[styles.n23mFooter, 'n23mFooter', className].filter(Boolean).join(' ')}
       {...props}
       style={{
         ...getCssVariableStyle({
-          '--footer': 'test',
+          '--bc': backgroundColor,
         }),
         ...style,
       }}
@@ -24,13 +37,13 @@ export const Footer: React.FC<FooterProps> = ({ children, className, style, ...p
       <div className={styles.n23mFooterContainer}>
         <Grid layout="container" widthMax={'1600px'}>
           <Grid layout="flex" widthMax={'1400px'} padding={'20px 0 0'} justifyContent="center">
-            <TextBox mobileSize={22} desktopSize={22} color="black" tag="p">
-              © Mariusz Najwer
+            <TextBox mobileSize={22} desktopSize={22} color={color} tag="p">
+              © {author}
             </TextBox>
           </Grid>
           <Grid layout="flex" widthMax={'1400px'} padding={'0'} justifyContent="center">
-            <TextBox mobileSize={12} desktopSize={12} color="black" tag="p">
-              2019 - {new Date().getFullYear()}
+            <TextBox mobileSize={12} desktopSize={12} color={color} tag="p">
+              {initialYear} - {new Date().getFullYear()}
             </TextBox>
           </Grid>
         </Grid>
