adf3a8999cb36117db7348376f83d18454b50e5d
Mariusz Najwer <najwer23@gmail.com>
2026-04-12 13:16:09 +0200

    Updates

--- assets/Calendar.stories-B1xnNRgd.js
+++ assets/Calendar.stories-B1xnNRgd.js
@@ -1,11 +1,14 @@
 import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-DnaggZxp.js";import{n,t as r}from"./calendar-DjS5jRM6.js";var i,a,o,s;e((()=>{r(),i=t(),a={title:`Input/Calendar`,component:n,parameters:{layout:`centered`,docs:{description:{component:`
-Test Test
+A simple calendar component for selecting and displaying dates.
 
 \`\`\`tsx
 import { Calendar } from 'najwer23morsels/lib/calendar';
 \`\`\`
-      `}}},tags:[`autodocs`]},o={args:{},render:e=>(0,i.jsx)(`div`,{children:(0,i.jsx)(n,{...e,children:`hello world!`})})},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {},
+      `}}},tags:[`autodocs`]},o={args:{name:`Calendar`,label:`Calendar`},render:e=>(0,i.jsx)(`div`,{children:(0,i.jsx)(n,{...e,children:`hello world!`})})},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: 'Calendar',
+    label: 'Calendar'
+  },
   render: args => <div>
       <Calendar {...args}>hello world!</Calendar>
     </div>
