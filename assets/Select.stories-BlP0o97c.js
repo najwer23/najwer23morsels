395e96ab30e622a87f7f6e7c0a94c5c2c963290f
Mariusz Najwer <najwer23@gmail.com>
2026-04-13 19:47:18 +0200

    Updates

--- assets/Select.stories-BlP0o97c.js
+++ assets/Select.stories-BlP0o97c.js
@@ -1,4 +1,4 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-DnaggZxp.js";import{n,t as r}from"./select-SCxWfXtV.js";var i,a,o,s,c;e((()=>{r(),i=t(),{action:a}=__STORYBOOK_MODULE_ACTIONS__,o={title:`Input/Select`,component:n,parameters:{layout:`centered`,docs:{description:{component:`
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-DnaggZxp.js";import{n,t as r}from"./select-DUEJucW8.js";var i,a,o,s,c;e((()=>{r(),i=t(),{action:a}=__STORYBOOK_MODULE_ACTIONS__,o={title:`Input/Select`,component:n,parameters:{layout:`centered`,docs:{description:{component:`
 Custom select/autocomplete input with a hidden submitted value.
 
 \`\`\`tsx
